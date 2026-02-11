14c6471cd92e0ff6c08bd6927f9dbe92edf99c14
rochisnag <rochisna.g@tektalis.com>
2026-02-11 10:19:31 +0530

    refactored

--- Employees/src/main/java/com/employee/main/EmployeeManagement.java
+++ Employees/src/main/java/com/employee/main/EmployeeManagement.java
@@ -1,16 +1,19 @@
 package com.employee.main;
-import com.employee.controller.MenuController;
+import com.employee.controller.Menu;
 import com.employee.dao.EmployeeDbDaoImpl;
 import com.employee.dao.EmployeeFileDaoImpl;
 import com.employee.util.StorageTypes;
 import java.util.Scanner;
 
+import org.mindrot.jbcrypt.BCrypt;
 
+import com.employee.dao.EmployeeDao;
+import com.employee.util.DatabaseConfig;
 public class EmployeeManagement {
     public static void main(String[] args) {
-        StorageTypes type = null;
         Scanner sc = new Scanner(System.in);
-        while (type == null) {
+        EmployeeDao  dao = null;
+        while (dao == null) {
             System.out.println("Select Storage Type:");
             for (StorageTypes types : StorageTypes.values()) {
                 System.out.println(types);
@@ -18,19 +21,27 @@ public class EmployeeManagement {
             System.out.println("Enter choice: ");
             String choice = sc.next().toUpperCase();
             try {
-                type = StorageTypes.valueOf(choice);
+               StorageTypes type = StorageTypes.valueOf(choice);
+               switch (type) {
+				case FILE:
+					dao=new EmployeeFileDaoImpl();
+					break;
+				case DATABASE:
+					DatabaseConfig.init(type);
+					dao=new EmployeeDbDaoImpl();
+					break;
+				case SUPABASE:
+					DatabaseConfig.init(type);
+					dao=new EmployeeDbDaoImpl();
+					break;
+				}
             } catch (IllegalArgumentException e) {
                 System.out.println("Invalid storage option");
             }
         }
-        MenuController menu = new MenuController();
-        switch (type) {
-            case FILE:
-                menu.displayMenu(new EmployeeFileDaoImpl());
-                break;
-            case POSTGRESQL:
-                menu.displayMenu(new EmployeeDbDaoImpl());
-                break;
-        }
+       
+    	while(true) {
+		Menu.displayMenu(dao);
+	}
     }
 }
